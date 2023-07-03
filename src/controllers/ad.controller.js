1955a4712b1cf05419efa3c502d9a92a41f69d86
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2023-07-03 11:34:55 +0100

    Hot fix

--- src/controllers/ad.controller.js
+++ src/controllers/ad.controller.js
@@ -1,4 +1,4 @@
-import Ad from "../models/Ad.js";
+import Ad from "../models/ad.model";
 import {
   createAdSchema,
   updateAdSchema,
@@ -6,77 +6,80 @@ import {
   deleteAdSchema,
 } from "../validators/ad.validator.js";
 
-export async function createAd(req, res) {
-  try {
-    const { error } = createAdSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
+export default class AdController {
+  static async createAd(req, res) {
+    try {
+      const { error } = createAdSchema.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+
+      const ad = new Ad(req.body);
+      await ad.save();
+
+      return res.status(201).json({ ad });
+    } catch (error) {
+      return res.status(500).json({ error: "Server error" });
     }
+  }
 
-    const ad = new Ad(req.body);
-    await ad.save();
+  static async updateAd(req, res) {
+    try {
+      const { error } = updateAdSchema.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
 
-    return res.status(201).json({ ad });
-  } catch (error) {
-    return res.status(500).json({ error: "Server error" });
+      const ad = await Ad.findByIdAndUpdate(req.params.adId, req.body, {
+        new: true,
+      });
+
+      if (!ad) {
+        return res.status(404).json({ error: "Ad not found" });
+      }
+
+      return res.status(200).json({ ad });
+    } catch (error) {
+      return res.status(500).json({ error: "Server error" });
+    }
+  }
+
+  static async getAd(req, res) {
+    try {
+      const { error } = getAdSchema.validate(req.params);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+
+      const ad = await Ad.findById(req.params.adId);
+
+      if (!ad) {
+        return res.status(404).json({ error: "Ad not found" });
+      }
+
+      return res.status(200).json({ ad });
+    } catch (error) {
+      return res.status(500).json({ error: "Server error" });
+    }
+  }
+
+  static async deleteAd(req, res) {
+    try {
+      const { error } = deleteAdSchema.validate(req.params);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+
+      const ad = await Ad.findByIdAndDelete(req.params.adId);
+
+      if (!ad) {
+        return res.status(404).json({ error: "Ad not found" });
+      }
+
+      return res.status(200).json({ message: "Ad deleted successfully" });
+    } catch (error) {
+      return res.status(500).json({ error: "Server error" });
+    }
   }
 }
 
-export async function updateAd(req, res) {
-  try {
-    const { error } = updateAdSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-
-    const ad = await Ad.findByIdAndUpdate(req.params.adId, req.body, {
-      new: true,
-    });
-
-    if (!ad) {
-      return res.status(404).json({ error: "Ad not found" });
-    }
-
-    return res.status(200).json({ ad });
-  } catch (error) {
-    return res.status(500).json({ error: "Server error" });
-  }
-}
-
-export async function getAd(req, res) {
-  try {
-    const { error } = getAdSchema.validate(req.params);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-
-    const ad = await Ad.findById(req.params.adId);
-
-    if (!ad) {
-      return res.status(404).json({ error: "Ad not found" });
-    }
-
-    return res.status(200).json({ ad });
-  } catch (error) {
-    return res.status(500).json({ error: "Server error" });
-  }
-}
-
-export async function deleteAd(req, res) {
-  try {
-    const { error } = deleteAdSchema.validate(req.params);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-
-    const ad = await Ad.findByIdAndDelete(req.params.adId);
-
-    if (!ad) {
-      return res.status(404).json({ error: "Ad not found" });
-    }
-
-    return res.status(200).json({ message: "Ad deleted successfully" });
-  } catch (error) {
-    return res.status(500).json({ error: "Server error" });
-  }
-}
